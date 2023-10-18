f0dfbb425bb8c6226d89d61737342033d054bb02
nicole-fabian <102332600+nicolefabian@users.noreply.github.com>
2023-10-19 12:17:46 +1300

    removed the duplicate retrieving function

--- GraduationCeremony/Controllers/HomeController.cs
+++ GraduationCeremony/Controllers/HomeController.cs
@@ -116,6 +116,11 @@ namespace GraduationCeremony.Controllers
             return View("ImportExcel");
         }
 
+        //retrieving the actual values by row
+        private string GetValue(ExcelWorksheet worksheet, int row, int columnIndex)
+        {
+            return worksheet.Cells[row, columnIndex].Text;
+        }
         //-----------------------Graduand
         private Graduand ExtractGraduand(ExcelWorksheet worksheet, int row)
         {
@@ -123,45 +128,36 @@ namespace GraduationCeremony.Controllers
             Graduand graduand = new Graduand
             {
                 //numbers in last are the column numbers for each
-                PersonCode = int.Parse(GetGraduandValue(worksheet, row, 1)),
-                Forenames = GetGraduandValue(worksheet, row, 2),
-                Surname = GetAwardValue(worksheet, row, 3),
-                Nsn = int.Parse(GetGraduandValue(worksheet, row, 4)),
-                Status = GetAwardValue(worksheet, row, 16),
-                BadDebtStatus = GetGraduandValue(worksheet, row, 17),
-                DateOfBirth = DateTime.Parse(GetGraduandValue(worksheet, row, 18)),
-                Ethnicity1 = GetGraduandValue(worksheet, row, 19),
-                Ethnicity2 = GetGraduandValue(worksheet, row, 20),
-                Ethnicity3 = GetGraduandValue(worksheet, row, 21),
-                Iwi1 = GetGraduandValue(worksheet, row, 22),
-                Iwi2 = GetGraduandValue(worksheet, row, 23),
-                Iwi3 = GetGraduandValue(worksheet, row, 24),
-                AddressLine1 = GetGraduandValue(worksheet, row, 25),
-                AddressLine2 = GetGraduandValue(worksheet, row, 26),
-                AddressLine3 = GetGraduandValue(worksheet, row, 27),
-                AddressLine4 = GetGraduandValue(worksheet, row, 28),
-                Town = GetGraduandValue(worksheet, row, 29),
-                Postcode = GetGraduandValue(worksheet, row, 30),
-                CollegeEmail = GetGraduandValue(worksheet, row, 31),
-                PersonalEmail = GetGraduandValue(worksheet, row, 32),
-                Mobile = GetGraduandValue(worksheet, row, 33),
-                Telephone = GetGraduandValue(worksheet, row, 34),
-                Campus = GetGraduandValue(worksheet, row, 35),
-                School = GetGraduandValue(worksheet, row, 36),
-                Comments = GetGraduandValue(worksheet, row, 39),
-                DateRecordAddedToMasterList = GetGraduandValue(worksheet, row, 40),
+                PersonCode = int.Parse(GetValue(worksheet, row, 1)),
+                Forenames = GetValue(worksheet, row, 2),
+                Surname = GetValue(worksheet, row, 3),
+                Nsn = int.Parse(GetValue(worksheet, row, 4)),
+                Status = GetValue(worksheet, row, 16),
+                BadDebtStatus = GetValue(worksheet, row, 17),
+                DateOfBirth = DateTime.Parse(GetValue(worksheet, row, 18)),
+                Ethnicity1 = GetValue(worksheet, row, 19),
+                Ethnicity2 = GetValue(worksheet, row, 20),
+                Ethnicity3 = GetValue(worksheet, row, 21),
+                Iwi1 = GetValue(worksheet, row, 22),
+                Iwi2 = GetValue(worksheet, row, 23),
+                Iwi3 = GetValue(worksheet, row, 24),
+                AddressLine1 = GetValue(worksheet, row, 25),
+                AddressLine2 = GetValue(worksheet, row, 26),
+                AddressLine3 = GetValue(worksheet, row, 27),
+                AddressLine4 = GetValue(worksheet, row, 28),
+                Town = GetValue(worksheet, row, 29),
+                Postcode = GetValue(worksheet, row, 30),
+                CollegeEmail = GetValue(worksheet, row, 31),
+                PersonalEmail = GetValue(worksheet, row, 32),
+                Mobile = GetValue(worksheet, row, 33),
+                Telephone = GetValue(worksheet, row, 34),
+                Campus = GetValue(worksheet, row, 35),
+                School = GetValue(worksheet, row, 36),
+                Comments = GetValue(worksheet, row, 39),
+                DateRecordAddedToMasterList = GetValue(worksheet, row, 40),
             };
             return graduand;
         }
-
-        //retrieving the actual values by row
-        private string GetGraduandValue(ExcelWorksheet worksheet, int row, int columnIndex)
-        {
-            return worksheet.Cells[row, columnIndex].Text;
-        }
-
-
-
 
         //-----------------------Award
         // Extract an Award from a row in the worksheet
@@ -171,20 +167,14 @@ namespace GraduationCeremony.Controllers
             //creating new Award object to store each one
             Award award = new Award
             {
-                AwardCode = GetAwardValue(worksheet, row, 5),
-                QualificationCode = GetAwardValue(worksheet, row, 6),
-                AwardDescription = GetAwardValue(worksheet, row, 7),
-                Level = GetAwardValue(worksheet, row, 8),
-                Credits = int.Parse(GetAwardValue(worksheet, row, 11)),
-                School = GetAwardValue(worksheet, row, 36)
+                AwardCode = GetValue(worksheet, row, 5),
+                QualificationCode = GetValue(worksheet, row, 6),
+                AwardDescription = GetValue(worksheet, row, 7),
+                Level = GetValue(worksheet, row, 8),
+                Credits = int.Parse(GetValue(worksheet, row, 11)),
+                School = GetValue(worksheet, row, 36)
             };
             return award;
-        }
-
-        //retrieving the actual values by row
-        private string GetAwardValue(ExcelWorksheet worksheet, int row, int columnIndex)
-        {
-            return worksheet.Cells[row, columnIndex].Text;
         }
 
         // Validation method for Award 
